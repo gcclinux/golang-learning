46fcd8efac8ee0cec8acc1fc1de66c50c8eab72d
Ricardo Wagemaker <java@wagemaker.co.uk>
2022-12-10 17:36:23 +0000

    10 Dec 2022 17:36:23

---
+++ rock-paper-scissors/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"time"
+)
+
+// Always type constants in all UPPER case letters
+const (
+	ROCK     = 0
+	PAPER    = 1
+	SCISSORS = 2
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	playerChoice := ""
+	playerValue := -1
+
+	computerValue := rand.Intn(2)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	clearScreen()
+
+	fmt.Print("Please enter rock, paper, or scissors -> ")
+	playerChoice, _ = reader.ReadString('\n')
+	playerChoice = strings.Replace(playerChoice, "\n", "", -1)
+
+	// First option and way of doing...
+	if playerChoice == "rock" {
+		playerValue = ROCK
+	}
+	if playerChoice == "paper" {
+		playerValue = PAPER
+	}
+	if playerChoice == "scissors" {
+		playerValue = SCISSORS
+	}
+	fmt.Println()
+	fmt.Println("Player chose", playerChoice, "and value is", playerValue)
+	fmt.Println("Computer chose", computerValue)
+}
+
+// clearScreen clears the screen
+func clearScreen() {
+	if strings.Contains(runtime.GOOS, "windows") {
+		// windows
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else {
+		// linux or mac
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
