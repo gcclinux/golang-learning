3356b513b725fd74b8aeb6c190f2644bbd3f119f
Ricardo Wagemaker <java@wagemaker.co.uk>
2023-01-03 22:20:40 +0000

    Tue  3 Jan 2023 22:20:40 GMT

--- web-game/rps/rps.go
+++ web-game/rps/rps.go
@@ -6,18 +6,33 @@ import (
 )
 
 const (
-	ROCK         = 0 // beats scissors. (scissors + 1) % 3 = 0
-	PAPER        = 1 // beats rock. (rock + 1) % 3 = 1
-	SCISSORS     = 2 // beats paper. (paper + 1) % 3 = 2
-	PLAYERWINS   = 1
-	COMPUTERWINS = 2
-	DRAW         = 3
+	ROCK     = 0 // beats scissors. (scissors + 1) % 3 = 0
+	PAPER    = 1 // beats rock. (rock + 1) % 3 = 1
+	SCISSORS = 2 // beats paper. (paper + 1) % 3 = 2
 )
 
 type Round struct {
+	Message        string `json:"message"`
 	Winner         int    `json:"winner"`
 	ComputerChoice string `json:"computer_choice"`
 	RoundResult    string `json:"round_result"`
+}
+
+var loseMessages = []string{
+	"Too bad!",
+	"Try again!",
+	"This is just not your day.",
+}
+var winMessages = []string{
+	"Good job!",
+	"Nice Work!",
+	"You should buy a lottery ticker!",
+}
+
+var drawMessages = []string{
+	"Great minds think alike!",
+	"Uh oh. Try again.",
+	"Nobody wins, but you can try again.!",
 }
 
 func PlayRound(playerValue int) Round {
@@ -25,31 +40,33 @@ func PlayRound(playerValue int) Round {
 	computerValue := rand.Intn(3)
 	computerChoice := ""
 	roundResult := ""
-	winner := 0
 
 	switch computerValue {
 	case ROCK:
 		computerChoice = "Computer chose ROCK"
-		winner = DRAW
 	case PAPER:
 		computerChoice = "Computer chose PAPER"
-		winner = PLAYERWINS
 	case SCISSORS:
 		computerChoice = "Computer chose SCISSORS"
-		winner = COMPUTERWINS
 	default:
 	}
 
+	messageInt := rand.Intn(3)
+	message := ""
+
 	if playerValue == computerValue {
 		roundResult = "It's a draw"
+		message = drawMessages[messageInt]
 	} else if playerValue == (computerValue+1)%3 {
 		roundResult = "Player Wins"
+		message = winMessages[messageInt]
 	} else {
 		roundResult = "Computer Wins"
+		message = loseMessages[messageInt]
 	}
 
 	var result Round
-	result.Winner = winner
+	result.Message = message
 	result.ComputerChoice = computerChoice
 	result.RoundResult = roundResult
 	return result
